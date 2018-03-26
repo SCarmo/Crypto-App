1201d2d5a877ac7eb48c4563d0d48fb8bdd1c66e
SCarmo <scarmody@tcd.ie>
2018-03-26 13:45:22 +0100

    added sjcl.js

--- popup.js
+++ popup.js
@@ -1,27 +1,24 @@
-if (window.location.href.indexOf("twitter.com") > -1) {
-    $("div.js-tweet-text-container > p").text("ERROR");
+if (window.location.href.indexOf("twitter.com") > -1) { // only work on twitter
+    //$("div.js-tweet-text-container > p").text("ERROR");
+
+    // for each tweet
+    $('div.js-tweet-text-container > p').each(function (index) {
+        //encrypt all the tweets and shit
+    });
 }
-
-function check(form) {
-    /*the following code checkes whether the entered userid and password are matching*/
-    if (form.userid.value == "shane" && form.pswrd.value == "gay") {
-        alert("correct")
-    } else {
-        alert("Error Password or Username") /*displays error message*/
-    }
-}
-
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
     var checkCredentials = document.getElementById('checkCredentials');
     checkCredentials.addEventListener('click', function () {
         var usr = document.getElementById("username").value;
         var psw = document.getElementById("password").value;
-        if (usr == "shane" && psw == "gay") {
-            alert("correct");
-            
+        if (usr == psw) { 
+            var ciphertext = sjcl.encrypt("password", "Hello World!");
+            var plaintext = sjcl.decrypt("password", ciphertext);
+
+            console.log(ciphertext);
+            console.log(plaintext);
+
         } else {
             alert("incorrect");
         }
