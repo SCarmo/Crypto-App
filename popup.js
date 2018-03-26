72ec47957607c0ab578125ff82b439b219bf8578
SCarmo <scarmody@tcd.ie>
2018-03-26 12:46:34 +0100

    check username and password

--- popup.js
+++ popup.js
@@ -1,3 +1,30 @@
-if(window.location.href.indexOf("twitter.com") > -1) {
+if (window.location.href.indexOf("twitter.com") > -1) {
     $("div.js-tweet-text-container > p").text("ERROR");
- }
+}
+
+function check(form) {
+    /*the following code checkes whether the entered userid and password are matching*/
+    if (form.userid.value == "shane" && form.pswrd.value == "gay") {
+        alert("correct")
+    } else {
+        alert("Error Password or Username") /*displays error message*/
+    }
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var checkCredentials = document.getElementById('checkCredentials');
+    checkCredentials.addEventListener('click', function () {
+        var usr = document.getElementById("username").value;
+        var psw = document.getElementById("password").value;
+        if (usr == "shane" && psw == "gay") {
+            alert("correct");
+            
+        } else {
+            alert("incorrect");
+        }
+
+    }, false);
+}, false);
